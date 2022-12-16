0c28b1286ad4c631afefbb78ad92491b575e5e12
heesoo <11junghs@gmail.com>
2022-12-16 16:20:08 +0900

    feat : layout update

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,13 +1,26 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 interface LayoutProps {
     children?: JSX.Element|JSX.Element[];
 }
 
 export default function Layout({children} : LayoutProps){
+    const router = useRouter();
     return (
         <>
-            CEOS 16기 투표
+            <Link
+            href="/">
+                CEOS 16기 투표
+            </Link>
+            {
+                router.pathname === "/" && 
+                <Link
+                href="/login">
+                    로그인
+                </Link>
+            }
             <div className="container">
                 {children}
             </div>
