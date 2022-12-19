c7d5351ea16a678c4b7137ffd8aa7ca9a9162dbf
seondal <sseondal@gmail.com>
2022-12-20 08:16:23 +0900

    ⚙️ set :  next-auth 를 이용한 카카오 로그인 구현

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth, { DefaultSession } from "next-auth"
+import KakaoProvider from "next-auth/providers/kakao"
+
+declare module "next-auth" {
+    interface Session {
+      user: {
+        email?: string
+        userId?: string
+      } & DefaultSession["user"]
+    }
+  }
+
+export default NextAuth({
+    providers:[
+        KakaoProvider({
+            clientId: process.env.KAKAO_CLIENT_ID!,
+            clientSecret: process.env.KAKAO_CLIENT_SECRET!
+        })
+    ],
+    callbacks: {
+        session({session, token}) {
+          session.user.userId = token.sub
+          return session
+        }
+      },
+})
--- pages/login.tsx
+++ pages/login.tsx
@@ -6,40 +6,41 @@ import axios from "axios";
 // redux test 부분
 import { login, selectUser } from "../src/features/user/userSlice";
 import { useSelector, useDispatch } from "react-redux";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 // redux test 부분
 
-
 export default function Login() {
+  const session = useSession();
 
-  const userEmail = useInput('');
-  const password = useInput('');
+  const userEmail = useInput("");
+  const password = useInput("");
 
   // redux test 부분
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
   // redux test 부분
 
-  const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(`
     userEmail : ${userEmail.value}
     password: ${password.value}
-    `)
+    `);
 
     const data = {
-      userid:userEmail.value,
-      password:password.value
-    }
+      userid: userEmail.value,
+      password: password.value,
+    };
 
     // redux test
     const sampleReduxData = {
-      useremail : userEmail.value,
-      password : password.value,
-    }
-    dispatch(login(sampleReduxData))
+      useremail: userEmail.value,
+      password: password.value,
+    };
+    dispatch(login(sampleReduxData));
 
-
-    //login post 
+    //login post
 
     // const response =  await (await fetch('/api/test/login',{
     //   method: 'POST',
@@ -49,43 +50,35 @@ export default function Login() {
     //   body: JSON.stringify(data)
     // })).json();
 
-    const response = await axios.post('/api/test/login', data)
+    const response = await axios.post("/api/test/login", data);
 
     const accessToken = response.data.token;
 
     // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
-    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+    axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
     console.log(accessToken);
 
     // accessToken을 localStorage, cookie 등에 저장하지 않는다 (XSS 취약점 보완)
+  };
 
-
-  }
-
-  const REST_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-  const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
-
-  const handleClick = () => {
-    window.location.href = KAKAO_AUTH_URL;
-  }
   return (
     <div>
-      CEOS 운영진 선출 투표 <br/>
+      CEOS 운영진 선출 투표 <br />
       <form onSubmit={handleSubmit}>
-        <input type="text" {...userEmail} placeholder="이메일"/><br/>
-        <input type="password" {...password} placeholder="비밀번호"/><br/>
+        <input type="text" {...userEmail} placeholder="이메일" />
+        <br />
+        <input type="password" {...password} placeholder="비밀번호" />
+        <br />
         <button type="submit">로그인</button>
       </form>
-      <br/>
-      <button onClick={handleClick}>
-        카카오 로그인
-      </button>
-      <br/>
-      <Link
-      href="/register">
-          회원가입
-      </Link>
+      <br />
+      {session.status === "unauthenticated" ? (
+        <button onClick={() => signIn("kakao")}>카카오 로그인</button>
+      ) : (
+        <button onClick={() => signOut()}>카카오 로그아웃</button>
+      )}
+      <br />
+      <Link href="/register">회원가입</Link>
     </div>
-  )
+  );
 }
