be52c9a19b24064e564ec765db87ca3513828978
seondal <sseondal@gmail.com>
2022-12-20 10:48:46 +0900

    ♻️ refactor :  카카오 로그인 관련 코드 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,28 +7,12 @@ import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 import store from "../src/store";
 
-//Kakao 함수를 전역에서 사용할 수 있도록 선언
-declare global {
-  interface Window {
-    Kakao: any;
-  }
-}
-
 export default function App({ Component, pageProps }: AppProps) {
-  // 페이지가 로드되면 실행
-  function kakaoInit() {
-    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
-    console.log(window.Kakao.isInitialized());
-  }
   return (
     <Provider store={store}>
       <SessionProvider session={pageProps.session}>
         <Layout>
           <Component {...pageProps} />
-          <Script
-            src="https://developers.kakao.com/sdk/js/kakao.js"
-            onLoad={kakaoInit}
-          ></Script>
         </Layout>
       </SessionProvider>
     </Provider>
