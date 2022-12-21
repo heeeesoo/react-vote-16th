0c134b556ef6d7482d5d80bf6850fed93600d926
heesoo <11junghs@gmail.com>
2022-12-22 01:35:36 +0900

    feat : redux-persist 추가

--- src/store.tsx
+++ src/store.tsx
@@ -1,14 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 import counterSlice from './features/counter/counterSlice'
 import userSlice from './features/user/userSlice'
 
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+import thunk from 'redux-thunk'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const reducer = combineReducers({
+    counter: counterSlice.reducer,
+    user : userSlice.reducer
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 export function makeStore(){
     return configureStore({
-        reducer: {
-            counter: counterSlice.reducer,
-            user : userSlice.reducer
-        }
+        reducer: persistedReducer,
+        middleware: [thunk]
     })
 }
 
@@ -17,3 +31,5 @@ const store = makeStore()
 export type AppState = ReturnType<typeof store.getState>
 
 export default store;
+
+export const persistor = persistStore(store);
